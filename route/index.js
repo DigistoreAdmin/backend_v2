bcb17e4e5d9d0698b33ea4bc46640a6e43def2e6
Aravind P <aravindparavind10@gmail.com>
2024-08-16 12:22:35 +0530

    fssaiRegistration

--- route/index.js
+++ route/index.js
@@ -22,9 +22,8 @@ const udyamRoute = require("../route/udyamRoute")
 const passportRoute = require("../route/passportRoute");
 const kswiftRoute = require("../route/kswiftRoute");
 const staffRoute =require("../route/staffRoute")
-const busBookingRoute = require("../route/busRoutes")
 const pancardRoute = require('../route/pancardRoute')
-
+const fssaiRegistrationRoute = require('../route/fssaiRegistrationRoute')
 
 
 const router = express.Router();
@@ -51,9 +50,8 @@ router.use("/v1/udyam", udyamRoute);
 router.use("/v1/passport", passportRoute);
 router.use("/v1/kswift", kswiftRoute);
 router.use("/v1/staff", staffRoute);
-router.use("/v1/bus", busBookingRoute)
 router.use("/v1/pancard", pancardRoute)
-
+router.use("/v1/fssai",fssaiRegistrationRoute)
 
 
 module.exports = router;    
