cee6d17ca8b42f86e6fb2bad6472c79c561cd576
kiranjithhardeepp <Kiranjithardeep@gmail.com>
2024-08-19 16:52:53 +0530

    cibil report

---
+++ db/migrations/20240819104026-create-cibil-report.js
@@ -0,0 +1,70 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("cibilReport", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      uniqueId: {
+        type: Sequelize.STRING,
+      },
+      customerName: {
+        type: Sequelize.STRING,
+      },
+      mobileNumber: {
+        type: Sequelize.BIGINT,
+      },
+      emailId: {
+        type: Sequelize.STRING,
+      },
+      purpose: {
+        type: Sequelize.ENUM("applyForCivilReport", "alreadyHaveCibilReport"),
+      },
+      cibilScore: {
+        type: Sequelize.BIGINT,
+      },
+      cibilReport: {
+        type: Sequelize.STRING,
+      },
+      aadhaarFront: {
+        type: Sequelize.STRING,
+      },
+      aadhaarBack: {
+        type: Sequelize.STRING,
+      },
+      panCardFront: {
+        type: Sequelize.STRING,
+      },
+      panCardBack: {
+        type: Sequelize.STRING,
+      },
+      status: {
+        type: Sequelize.ENUM("approve", "pending", "reject", "process"),
+      },
+      assignedId: {
+        type: Sequelize.STRING,
+      },
+      assignedOn: {
+        type: Sequelize.DATE,
+      },
+      completedOn: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("cibilReport");
+  },
+};
