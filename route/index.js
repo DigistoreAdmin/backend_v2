bf122cc4ef6b5d6a922304cd988cb553e99605ba
safeer239 <mohammedsafeershefi@gamil.com>
2024-08-28 16:40:02 +0530

    safeer new business loan unsecured

--- route/index.js
+++ route/index.js
@@ -25,12 +25,11 @@ const staffRoute = require("../route/staffRoute")
 const busBookingRoute = require("../route/busRoutes")
 const pancardRoute = require('../route/pancardRoute')
 const gstFilingRoute = require('../route/gstFilingRoute')
-const pancardRoute = require('../route/pancardRoute')
 const gstRegistrationRoute = require('../route/gstRegistrationRoutes')
 const financialStatementRoute = require("../route/financialStatementRoute");
 const companyFormationRoute = require("../route/companyFormationRoute");
 const getPancardRoute = require('../route/getPancardRoute')
-
+const businessLoanNewUnsecured =require('../route/Loan/businessLoanNewUnsecuredRouter')
 
 
 
@@ -68,7 +67,7 @@ router.use("/v1/gst", gstRegistrationRoute)
 router.use("/v1/financialStatementRoute",financialStatementRoute)
 router.use("/v1/companyFormationRoute",companyFormationRoute)
 router.use("/v1/getPancard", getPancardRoute)
-
+router.use("/v1/businessLoanNewUnsecuredRoute", businessLoanNewUnsecured)
 
 
 module.exports = router;    
