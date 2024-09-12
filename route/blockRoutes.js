11002fc238c51f9d87276eccd3924047666438fc
amalsaj <amalsaji218@gmail.com>
2024-09-12 10:52:39 +0530

    staff access controller

--- route/blockRoutes.js
+++ route/blockRoutes.js
@@ -1,8 +1,22 @@
 const { restrictTo } = require("../controller/authController");
-const { updateFranchiseBlock } = require("../controller/franchiseBlockController");
-const { verifyToken, verifyRefreshToken } = require('../utils/token')
+const {
+  updateFranchiseBlock,
+} = require("../controller/franchiseBlockController");
+const { staffAccess } = require("../controller/staffAccessController");
+
+const { verifyToken, verifyRefreshToken } = require("../utils/token");
 const router = require("express").Router();
 
-router.route("/updateFranchiseBlock").put(verifyToken, verifyRefreshToken, restrictTo("admin"), updateFranchiseBlock);
+router
+  .route("/updateFranchiseBlock")
+  .put(
+    verifyToken,
+    verifyRefreshToken,
+    restrictTo("admin"),
+    updateFranchiseBlock
+  );
+router
+  .route("/updateStaffAccess")
+  .put(verifyToken, verifyRefreshToken, restrictTo("admin"), staffAccess);
 
 module.exports = router;
