b2bba257b7f2fb780d38a46a033703fab1073ea5
kiranjithhardeepp <Kiranjithardeep@gmail.com>
2024-09-13 10:28:24 +0530

    updated get request in vehicle loan

--- route/Loan/vehicleLoanRouter.js
+++ route/Loan/vehicleLoanRouter.js
@@ -1,15 +1,10 @@
 const {
   createNewVehicleLoan,
 } = require("../../controller/Loan/vehicleLoanNewController");
-const {
-  getAllNewVehicleLoans,
-} = require("../../controller/Loan/getVehicleLoanNewController");
+
 const {
   createUsedVehicleLoan,
 } = require("../../controller/Loan/vehicleLoanUsedController");
-const {
-  getAllUsedVehicleLoans,
-} = require("../../controller/Loan/getVehicleLoanUsedController");
 
 const { verifyToken, verifyRefreshToken } = require("../../utils/token");
 const router = require("express").Router();
@@ -19,15 +14,7 @@ router
   .post(verifyToken, verifyRefreshToken, createNewVehicleLoan);
 
 router
-  .route("/newVehicleLoan")
-  .get(verifyToken, verifyRefreshToken, getAllNewVehicleLoans);
-
-router
   .route("/usedVehicleLoan")
   .post(verifyToken, verifyRefreshToken, createUsedVehicleLoan);
-
-router
-  .route("/usedVehicleLoan")
-  .get(verifyToken, verifyRefreshToken, getAllUsedVehicleLoans);
 
 module.exports = router;
