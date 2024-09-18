9bd852312ac35db6ec7be6c33ccbf75dae7c2ad3
AMAL SAJI <103961626+amalsaj@users.noreply.github.com>
2024-09-18 10:03:22 +0530

    staff registration completed

--- route/staffRoute.js
+++ route/staffRoute.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const { createStaffs } = require("../controller/staffController");
 
-router.route("/createStaff").post(createStaffs);
+const { verifyOTP, restrictTo } = require("../controller/authController");
+const { verifyToken, verifyRefreshToken } = require("../utils/token");
+
+router
+  .route("/createStaff")
+  .post(
+    verifyToken,
+    verifyRefreshToken,
+    restrictTo("admin"),
+    verifyOTP,
+    createStaffs
+  );
 
 module.exports = router;
