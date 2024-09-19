d6a44dd1a210919603e2d7b2c90d3857e7cff5a4
Aravind P <aravindparavind10@gmail.com>
2024-09-19 12:13:32 +0530

    Delete franchise and change staff update to adminEdit controller

--- route/index.js
+++ route/index.js
@@ -37,8 +37,8 @@ const gstRegistrationRoute = require('../route/gstRegistrationRoutes')
 const financialStatementRoute = require("../route/financialStatementRoute");
 const companyFormationRoute = require("../route/companyFormationRoute");
 const personalLoanRoute = require("../route/Loan/personalLoanRouter");
-const businessLoanNewSecuredRoute=require('../route/Loan/businessLoanNewSecuredRouter')
-const businessLoanNewUnsecured =require('../route/Loan/businessLoanNewUnsecuredRouter')
+const businessLoanNewSecuredRoute = require('../route/Loan/businessLoanNewSecuredRouter')
+const businessLoanNewUnsecured = require('../route/Loan/businessLoanNewUnsecuredRouter')
 const BusinessLoanUnsecuredExistingRouter = require('../route/Loan/BusinessLoanUnsecuredExistingRouter')
 const collegeTeamRoute = require('../route/collegeTeamRoute')
 const staffUpdateRouter = require('../route/staffUpdateRouter')
@@ -46,9 +46,9 @@ const partnerShipDeedPreperationRoute = require("../route/partnerShipDeedPrepera
 const BlockRoute = require("../route/blockRoutes")
 const fetchLoanRouter = require("../route/Loan/fetchLoanRouter");
 const fetchServicesRouter = require("../route/fetchServicesRouter");
+const adminEditRoute = require("../route/adminEditRoute")
 
 const router = express.Router();
-
 
 router.use("/v1/auth", authRouter);
 router.use("/v1/projects", projectRouter);
@@ -79,23 +79,23 @@ router.use("/v1/bus", busBookingRoute);
 router.use("/v1/cibilReportRoute", cibilReportRoute);
 router.use("/v1/pancard", pancardRoute)
 router.use("/v1/loan", loanAgainstPropertyRoute)
-router.use("/v1/forgotPassword",forgotPasswordRoute)
-router.use("/v1/fssai",fssaiLicenceRoute)
-router.use("/v1/fssai",fssaiRegistrationRoute)
+router.use("/v1/forgotPassword", forgotPasswordRoute)
+router.use("/v1/fssai", fssaiLicenceRoute)
+router.use("/v1/fssai", fssaiRegistrationRoute)
 router.use('/v1/gst/', gstFilingRoute)
 router.use("/v1/gst", gstRegistrationRoute)
-router.use("/v1/financialStatementRoute",financialStatementRoute)
-router.use("/v1/companyFormationRoute",companyFormationRoute)
-router.use("/v1/personalLoanRoute",personalLoanRoute)
+router.use("/v1/financialStatementRoute", financialStatementRoute)
+router.use("/v1/companyFormationRoute", companyFormationRoute)
+router.use("/v1/personalLoanRoute", personalLoanRoute)
 router.use("/v1/businessLoanNewSecured", businessLoanNewSecuredRoute)
 router.use("/v1/businessLoanNewUnsecuredRoute", businessLoanNewUnsecured)
 router.use("/v1/BusinessLoanUnsecuredExisting", BusinessLoanUnsecuredExistingRouter)
 router.use("/v1/students", collegeTeamRoute)
 router.use("/v1/staffUpdateRouter", staffUpdateRouter)
-router.use("/v1/partnerShipDeedPreperationRoute",partnerShipDeedPreperationRoute);
+router.use("/v1/partnerShipDeedPreperationRoute", partnerShipDeedPreperationRoute);
 router.use("/v1/updateAccess", BlockRoute)
 router.use("/v1/fetchLoanRouter", fetchLoanRouter);
 router.use("/v1/fetchServicesRouter", fetchServicesRouter);
-
+router.use("/v1/adminEditRoute", adminEditRoute)
 
 module.exports = router;
