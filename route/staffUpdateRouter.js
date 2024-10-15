efca47bbd1363fe754b3ada51e64476753afa97c
kiranjithhardeepp <Kiranjithardeep@gmail.com>
2024-10-15 16:19:25 +0530

    passport update

--- route/staffUpdateRouter.js
+++ route/staffUpdateRouter.js
@@ -1,22 +1,42 @@
 const express = require("express");
-const { loanStatus, trainBookingUpdate, updatePanDetails, updateGstDetails, updateInsuranceDetails, incometaxUpdate } = require("../controller/staffUpdateController");
+const {
+  loanStatus,
+  trainBookingUpdate,
+  updatePanDetails,
+  updateGstDetails,
+  updateInsuranceDetails,
+  incometaxUpdate,
+  passportUpdate,
+} = require("../controller/staffUpdateController");
 
 const { verifyRefreshToken, verifyToken } = require("../utils/token");
 
 const router = express.Router();
 
-router.route("/loanStatus").put(verifyToken, verifyRefreshToken,loanStatus)
+router.route("/loanStatus").put(verifyToken, verifyRefreshToken, loanStatus);
 
-router.route("/updateTrainBooking").put(verifyToken,verifyRefreshToken,trainBookingUpdate)
+router
+  .route("/updateTrainBooking")
+  .put(verifyToken, verifyRefreshToken, trainBookingUpdate);
 
-router.route("/updateGstDetails").put(verifyToken, verifyRefreshToken, updateGstDetails);
+router
+  .route("/updateGstDetails")
+  .put(verifyToken, verifyRefreshToken, updateGstDetails);
 
-router.route("/updatePanDetails").put(verifyToken, verifyRefreshToken, updatePanDetails);
+router
+  .route("/updatePanDetails")
+  .put(verifyToken, verifyRefreshToken, updatePanDetails);
 
-router.route("/updateInsuranceDetails").put(verifyToken, verifyRefreshToken, updateInsuranceDetails);
+router
+  .route("/updateInsuranceDetails")
+  .put(verifyToken, verifyRefreshToken, updateInsuranceDetails);
 
-router.route("/incometaxUpdate").put(verifyToken, verifyRefreshToken,incometaxUpdate)
+router
+  .route("/incometaxUpdate")
+  .put(verifyToken, verifyRefreshToken, incometaxUpdate);
+
+router
+  .route("/passportUpdate")
+  .put(verifyToken, verifyRefreshToken, passportUpdate);
 
 module.exports = router;
-
-
