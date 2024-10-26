0d07cc3ee95487cfab9f4624aed3124d4220916c
arunak007-sys <arun2017ak@gmail.com>
2024-10-26 14:55:29 +0530

    pancard update in staff Update Controller  moved to pancard controller

--- route/staffUpdateRouter.js
+++ route/staffUpdateRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { loanStatus, trainBookingUpdate, updatePanDetails, updateGstDetails, updateInsuranceDetails, incometaxUpdate, updateBusBooking } = require("../controller/staffUpdateController");
+const { loanStatus, trainBookingUpdate, updateGstDetails, updateInsuranceDetails, incometaxUpdate, updateBusBooking } = require("../controller/staffUpdateController");
 
 const { verifyRefreshToken, verifyToken } = require("../utils/token");
 
@@ -12,8 +12,6 @@ router.route("/updateTrainBooking").put(verifyToken,verifyRefreshToken,trainBook
 router.route("/updateBusBooking").put(verifyToken, verifyRefreshToken, updateBusBooking)
 
 router.route("/updateGstDetails").put(verifyToken, verifyRefreshToken, updateGstDetails);
-
-router.route("/updatePanDetails").put(verifyToken, verifyRefreshToken, updatePanDetails);
 
 router.route("/updateInsuranceDetails").put(verifyToken, verifyRefreshToken, updateInsuranceDetails);
 
