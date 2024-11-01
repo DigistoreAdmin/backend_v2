043d5cc07a1ad57941932361701d8746099346f3
kiranjithhardeepp <Kiranjithardeep@gmail.com>
2024-11-01 12:13:08 +0530

    kiranjith vehicle loan new db update

---
+++ db/migrations/20240823101323-create-vehicle-loan-new.js
@@ -0,0 +1,130 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("vehicleLoan_New", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      uniqueId: {
+        type: Sequelize.STRING,
+      },
+      workId: {
+        type: Sequelize.STRING,
+      },
+      customerName: {
+        type: Sequelize.STRING,
+      },
+      phoneNumber: {
+        type: Sequelize.BIGINT,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      typeofLoan: {
+        type: Sequelize.ENUM("business", "salaried"),
+      },
+      salarySlip: {
+        type: Sequelize.STRING,
+      },
+      bankStatement: {
+        type: Sequelize.STRING,
+      },
+      cancelledCheque: {
+        type: Sequelize.STRING,
+      },
+      photo: {
+        type: Sequelize.STRING,
+      },
+      ITR: {
+        type: Sequelize.STRING,
+      },
+      rentAgreement: {
+        type: Sequelize.STRING,
+      },
+      panchayathOrMuncipleLicence: {
+        type: Sequelize.STRING,
+      },
+      invoiceFromDealer: {
+        type: Sequelize.STRING,
+      },
+      cibil: {
+        type: Sequelize.ENUM("approved", "noCibil"),
+      },
+      cibilScore: {
+        type: Sequelize.BIGINT,
+      },
+      cibilReport: {
+        type: Sequelize.STRING,
+      },
+      acknowledgment: {
+        type: Sequelize.STRING,
+      },
+      loanStatus: {
+        type: Sequelize.JSONB,
+        defaultValue: {
+          documentSubmittedToBank: false,
+          bankVerified: false,
+          bankApprovalOrReject: false,
+          loanDispersed: false,
+          commissionCredited: false
+        },
+      },
+      rejectReason: {
+        type: Sequelize.STRING
+      },
+      bankDetails: {
+        type: Sequelize.STRING
+      },
+      loanGivenByBank: {
+        type: Sequelize.BIGINT
+      },
+      doneBy: {
+        type: Sequelize.STRING
+      },
+      serviceCharge: {
+        type: Sequelize.INTEGER
+      },
+      commissionToFranchise: {
+        type: Sequelize.INTEGER
+      },
+      commissionToHO: {
+        type: Sequelize.INTEGER
+      },
+      otherPayments: {
+        type: Sequelize.STRING
+      },
+      otherDocumentsByStaff: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.ENUM("inQueue", "inProgress", "Completed"),
+        defaultValue: "inQueue",
+      },
+      assignedId: {
+        type: Sequelize.STRING,
+      },
+      assignedOn: {
+        type: Sequelize.DATE,
+      },
+      completedOn: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("vehicleLoan_New");
+  },
+};
