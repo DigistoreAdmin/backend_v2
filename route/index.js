052b2fd07cd59e6d936cc3d527024c237b7e6e8a
AMAL SAJI <103961626+amalsaj@users.noreply.github.com>
2024-11-07 17:10:12 +0530

    upiIntent controller completed

--- route/index.js
+++ route/index.js
@@ -57,6 +57,7 @@ const flightBookingRoute = require("../route/rechapi-routes/flightTicketRoute")
 const rechapiPhoneRechargeRoute = require("../route/rechapi-routes/phoneRechargeRoute")
 const rechapiBillPaymentRoute = require("../route/rechapi-routes/billPaymentRoute")
 const busBookingRechapi = require("../route/rechapi-routes/busBookingRoute")
+const upiIntentRoute = require("../route/upiPaymentRoute/upiIntentRoute")
 
 
 
@@ -120,6 +121,7 @@ router.use("/v1/flightBooking", flightBookingRoute)
 router.use("/v1/rechapiPhoneRechargeRoute",rechapiPhoneRechargeRoute)
 router.use("/v1/rechapiBillPaymentRoute",rechapiBillPaymentRoute)
 router.use("/v1/bus",busBookingRechapi )
+router.use("/v1/upiIntent",upiIntentRoute )
 
 
 
