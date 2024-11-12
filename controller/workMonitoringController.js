db88649f9df5a896b0962d4451e00a82f3d39cb7
arunak007-sys <arun2017ak@gmail.com>
2024-11-12 16:03:32 +0530

    changes staff work time model and controller

--- controller/workMonitoringController.js
+++ controller/workMonitoringController.js
@@ -1,13 +1,14 @@
-const workTime = require("../db/models/workTIme");
+const workTime = require("../db/models/worktime");
 const defineStaff = require("../db/models/staffs");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 const staffWorkTime = catchAsync(async (req, res, next) => {
   try {
     const { workId, status } = req.body;
 
-    if(!workId) {
-      return res.status(400).json({ error: "workId is required"})
+    if (!workId) {
+      return res.status(400).json({ error: "workId is required" });
     }
 
     const user = req.user;
@@ -28,150 +29,145 @@ const staffWorkTime = catchAsync(async (req, res, next) => {
     if (!workIdExist) {
       const workTimeInstance = await workTime.create({
         workId,
-        startTime: [new Date()],
-        staffName: [staffExists.firstName],
-        assignedId: [staffExists.employeeId],
-        endTime: [],
-        totalWorkTimeWithoutBreak: [],
+        employeeRecords: [
+          {
+            staffName: staffExists.firstName,
+            startTime: new Date(),
+          },
+        ],
       });
       return res.status(201).json({ workTimeInstance });
     }
 
-    if(workIdExist.reassigned === true) {
-      if (!workIdExist.assignedId.includes(staffExists.employeeId)) {
-        const startTime = new Date();
-        const updatedTime = [...(workIdExist.startTime || []), startTime];
-        // const updatedStaffName = [...(workIdExist.staffName || []), staffExists.firstName];
-        const updatedAssignedId = [...(workIdExist.assignedId || []), staffExists.employeeId];
-  
-        await workIdExist.update({
-          startTime: updatedTime,
-          // staffName: updatedStaffName,
-          assignedId: updatedAssignedId,
-        });
-  
-        const reassignedStaff = await workTime.findOne({
-          where: { workId: workIdExist.workId },
-        });
-  
-        return res.status(200).json({ reassignedStaff });
-      }
-    }
-
-    const index = workIdExist.assignedId.indexOf(staffExists.employeeId);
-
-    if (status === "breakStarted") {
-      const breakTimeStarted = new Date();
-      const updatedBreakStart = [...(workIdExist.breakTimeStarted || []), breakTimeStarted];
-
-      await workTime.update(
-        { breakTimeStarted: updatedBreakStart },
-        { where: { workId: workIdExist.workId } }
-      );
-
-      const updatedBreak = await workTime.findOne({
-        where: { workId: workIdExist.workId }
+    const targetRecord = workIdExist.employeeRecords.find(
+      (record) => record.staffName === staffExists.firstName
+    );
+    
+    const staffRecordIndex = workIdExist.employeeRecords.findIndex(
+      (record) => record.staffName === staffExists.firstName
+    );
+    
+    if (
+      targetRecord?.staffName === staffExists.firstName &&
+      targetRecord?.reassigned === true &&
+      targetRecord?.assignedId === staffExists.employeeId &&
+      !targetRecord?.startTime
+    ) {
+      const updatedRecords = workIdExist.employeeRecords.map((record, index) => {
+        if (index === staffRecordIndex) {
+          return {
+            ...record,
+            startTime: new Date(),
+          };
+        }
+        return record;
       });
-      return res.status(200).json({ updatedBreak });
-    }
-
-    if (status === "breakEnded") {
-      const breakTimeEnded = new Date();
-      const breakStartTimes = workIdExist.breakTimeStarted || [];
-      const lastBreakStart = new Date(breakStartTimes[breakStartTimes.length - 1]);
-
-      const breakDuration = breakTimeEnded - lastBreakStart;
-      const hoursTaken = Math.floor(breakDuration / (1000 * 60 * 60));
-      const minutesTaken = Math.floor((breakDuration % (1000 * 60 * 60)) / (1000 * 60));
-      const secondsTaken = Math.floor((breakDuration % (1000 * 60)) / 1000);
-      const breakDurationFormatted = `${hoursTaken}h ${minutesTaken}m ${secondsTaken}s`;
-
-      const updatedBreakEnd = [...(workIdExist.breakTimeEnded || []), breakTimeEnded];
-      const updatedTotalTime = [...(workIdExist.totalBreakTime || []), breakDurationFormatted];
-
-      await workTime.update(
-        {
-          breakTimeEnded: updatedBreakEnd,
-          totalBreakTime: updatedTotalTime,
-        },
-        { where: { workId: workIdExist.workId } }
-      );
-
-      const updatedBreak = await workTime.findOne({
-        where: { workId: workIdExist.workId }
+    
+      await workIdExist.update({
+        employeeRecords: updatedRecords,
       });
-
-      return res.status(200).json({ updatedBreak });
     }
+    
+
+    if(status ==="breakStarted"){
+      const updatedRecords = workIdExist.employeeRecords.map((record,index) => {
+          if (index === staffRecordIndex) {
+              return {
+                  ...record,
+                  breakTimeStarted: [...(record.breakTimeStarted || []), new Date()],
+              };
+          }
+          return record;
+      });
+  
+      await workIdExist.update({
+          employeeRecords: updatedRecords,
+      });
+  
+      return res
+          .status(200)
+          .json({ message: "Work time marked as completed", workIdExist });
+  }
+
+  if (status === "breakEnded") {
+    const updatedRecords = workIdExist.employeeRecords.map((record,index) => {
+    if (index === staffRecordIndex) {
+        const breakTimeEnded = [...(record.breakTimeEnded || []), new Date()];
+        const breakTime = (record.breakTime || []).slice();
+
+        if (record.breakTimeStarted && record.breakTimeStarted.length > 0) {
+        const lastBreakStartIndex = record.breakTimeStarted.length - 1;
+        const breakDuration = new Date(breakTimeEnded[breakTimeEnded.length - 1]) - new Date(record.breakTimeStarted[lastBreakStartIndex]);
+        const formatedDuration=formatDuration(breakDuration)
+        breakTime.push(formatedDuration);
+        }
+
+        const totalBreakTimeInMilliSec = breakTime.reduce((total, duration) => total + parseDuration(duration), 0);
+        const totalBreakTime = formatDuration(totalBreakTimeInMilliSec);
+
+        return {
+        ...record,
+        breakTimeEnded,
+        breakTime,
+        totalBreakTime              
+        };
+    }
+    return record;
+    });
+
+    await workIdExist.update({
+    employeeRecords: updatedRecords,
+    });
+
+    return res.status(200).json({ message: "Break time ended", workIdExist });
+}
 
     if (status === "completed") {
-      const endTime = new Date();
-      if (isNaN(endTime.getTime())) {
-        return res.status(400).json({ error: 'Invalid end time' });
-      }
-    
-      if (!workIdExist.endTime[index] || !workIdExist.totalWorkTimeWithoutBreak[index]) {
-        const startTime = workIdExist.startTime[index];
-        if (isNaN(new Date(startTime).getTime())) {
-          return res.status(400).json({ error: 'Invalid start time' });
-        }
-    
-        const totalTimeWithBreaks = endTime - new Date(startTime);
-    
-        let breakDuration = 0;
-        if (workIdExist.breakTimeStarted && workIdExist.breakTimeStarted.length > 0) {
-          for (let i = 0; i < workIdExist.breakTimeStarted.length; i++) {
-            const breakStart = new Date(workIdExist.breakTimeStarted[i]);
-            const breakEnd = new Date(workIdExist.breakTimeEnded[i]);
-    
-            if (isNaN(breakStart.getTime()) || isNaN(breakEnd.getTime())) {
-              return res.status(400).json({ error: 'Invalid break time' });
-            }
-    
-            if (breakStart >= new Date(startTime) && breakEnd <= endTime) {
-              breakDuration += breakEnd - breakStart;
-            }
+      const updatedRecords = workIdExist.employeeRecords.map((record,index) => {
+          if (index === staffRecordIndex && !record.endTime) {
+              const endTime=new Date()
+              const startTime = new Date(record.startTime)
+              const workDuration= endTime - startTime
+              const totalTimeTaken = formatDuration(workDuration)
+          return {
+              ...record,
+              endTime: new Date(),
+              totalTimeTaken
+          };
           }
-        }
-    
-        const totalTimeWithoutBreaks = totalTimeWithBreaks - breakDuration;
-        const hoursTaken1 = Math.floor(totalTimeWithoutBreaks / (1000 * 60 * 60));
-        const minutesTaken1 = Math.floor((totalTimeWithoutBreaks % (1000 * 60 * 60)) / (1000 * 60));
-        const secondsTaken1 = Math.floor((totalTimeWithoutBreaks % (1000 * 60)) / 1000);
-        const totalTimeWithoutBreak = `${hoursTaken1}h ${minutesTaken1}m ${secondsTaken1}s`;
-    
-        let totalTimeWithBreak = null;
-        if (breakDuration > 0) {
-          const hoursTaken = Math.floor(totalTimeWithBreaks / (1000 * 60 * 60));
-          const minutesTaken = Math.floor((totalTimeWithBreaks % (1000 * 60 * 60)) / (1000 * 60));
-          const secondsTaken = Math.floor((totalTimeWithBreaks % (1000 * 60)) / 1000);
-          totalTimeWithBreak = `${hoursTaken}h ${minutesTaken}m ${secondsTaken}s`;
-        }
-    
-        const updatedEndTime = [...(workIdExist.endTime || []), new Date(endTime)];
-        const updatedTotalTimeWithoutBreak = [...workIdExist.totalWorkTimeWithoutBreak, totalTimeWithoutBreak];
-        const updatedTotalTimeWithBreak = [...(workIdExist.totalWorkTimeWithBreak || []), totalTimeWithBreak];
-    
-        await workIdExist.update({
-          endTime: updatedEndTime,
-          totalWorkTimeWithoutBreak: updatedTotalTimeWithoutBreak,
-          totalWorkTimeWithBreak: updatedTotalTimeWithBreak,
-          totalBreakTime: workIdExist.totalBreakTime ? workIdExist.totalBreakTime : [...(workIdExist.totalBreakTime || []), totalTimeWithBreak],
-        });
+          return record;
+      });
+
+      await workIdExist.update({
+          employeeRecords: updatedRecords,
+      });
+
+      return res
+          .status(200)
+          .json({ message: "Work time marked as completed", workIdExist });
       }
-    
-      await workIdExist.save();
-      return res.status(200).json({ totalWorkTimeWithBreak: workIdExist });
-    }
-    
-    
-    
 
     return res.status(200).json({ workIdExist });
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json({ message: "An error occurred", error: error.message });
+    return res
+      .status(500)
+      .json({ message: "An error occurred", error: error.message });
   }
 });
+
+const formatDuration = (milliseconds) => {
+  const seconds = Math.floor((milliseconds / 1000) % 60);
+  const minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
+  const hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
+  return `${hours}h:${minutes}m:${seconds}s`;
+};
+
+const parseDuration = (formattedTime) => {
+  const [hours, minutes, seconds] = formattedTime
+    .split(/[hms:]+/)
+    .map(Number);
+  return hours * 3600000 + minutes * 60000 + seconds * 1000;
+};
 
 module.exports = { staffWorkTime };
--- db/migrations/20241029054014-create-work-time.js
+++ db/migrations/20241029054014-create-work-time.js
@@ -1,65 +1,138 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('workTime', {
+    await queryInterface.createTable("workTime", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       workId: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      staffName: {
-        type: Sequelize.ARRAY(Sequelize.STRING),
-      },
-      assignedId:{
-        type: Sequelize.ARRAY(Sequelize.STRING),
-      },
-      startTime: {
-        type: Sequelize.ARRAY(Sequelize.DATE),
-      },
-      endTime: {
-        type: Sequelize.ARRAY(Sequelize.DATE),
-      },
-      totalWorkTimeWithoutBreak: {
-        type: Sequelize.ARRAY(Sequelize.STRING),
-      },
-      breakTimeStarted: {
-        type: Sequelize.ARRAY(Sequelize.DATE),
-      },
-      breakTimeEnded: {
-        type: Sequelize.ARRAY(Sequelize.DATE),
-      },
-      totalBreakTime: {
-        type: Sequelize.ARRAY(Sequelize.STRING),
-      },
-      totalWorkTimeWithBreak:{
-        type: Sequelize.ARRAY(Sequelize.STRING),
-      },
-      reassigned: {
-        type: Sequelize.BOOLEAN,
-      },
-      reassignedTime: {
-        type: Sequelize.ARRAY(Sequelize.DATE),
+      employeeRecords: {
+        type: Sequelize.ARRAY(Sequelize.JSON),
+        defaultValue: [
+          {
+            staffName: {
+              type: Sequelize.STRING,
+            },
+            assignedId: {
+              type: Sequelize.STRING,
+            },
+            startTime: {
+              type: Sequelize.STRING,
+            },
+            endTime: {
+              type: Sequelize.STRING,
+            },
+            breakTimeStarted: {
+              type: Sequelize.ARRAY(Sequelize.STRING)
+            },
+            breakTimeEnded: {
+              type: Sequelize.ARRAY(Sequelize.DATE),
+            },
+            breakTime: {
+              type: Sequelize.ARRAY(Sequelize.STRING),
+            },
+            totalBreakTime: {
+              type: Sequelize.STRING,
+            },
+            totalTimeTaken: {
+              type: Sequelize.STRING,
+            },
+            reassigned: {
+              type: Sequelize.BOOLEAN,
+            },
+            reassignedTime: {
+              type: Sequelize.STRING,
+            },
+          },
+        ],
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       deletedAt: {
         type: Sequelize.DATE,
-      }, 
+      },
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('workTime');
-  }
+    await queryInterface.dropTable("workTime");
+  },
 };
+
+// 'use strict';
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.createTable('workTime', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       workId: {
+//         type: Sequelize.STRING
+//       },
+//       staffName: {
+//         type: Sequelize.ARRAY(Sequelize.STRING),
+//       },
+//       assignedId:{
+//         type: Sequelize.ARRAY(Sequelize.STRING),
+//       },
+//       startTime: {
+//         type: Sequelize.ARRAY(Sequelize.DATE),
+//       },
+//       endTime: {
+//         type: Sequelize.ARRAY(Sequelize.DATE),
+//       },
+//       totalWorkTimeWithoutBreak: {
+//         type: Sequelize.ARRAY(Sequelize.STRING),
+//       },
+//       breakTimeStarted: {
+//         type: Sequelize.ARRAY(Sequelize.DATE),
+//       },
+//       breakTimeEnded: {
+//         type: Sequelize.ARRAY(Sequelize.DATE),
+//       },
+//       totalBreakTime: {
+//         type: Sequelize.ARRAY(Sequelize.STRING),
+//       },
+//       totalWorkTimeWithBreak:{
+//         type: Sequelize.ARRAY(Sequelize.STRING),
+//       },
+//       reassigned: {
+//         type: Sequelize.BOOLEAN,
+//       },
+//       reassignedTime: {
+//         type: Sequelize.ARRAY(Sequelize.DATE),
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       deletedAt: {
+//         type: Sequelize.DATE,
+//       }, 
+//     });
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.dropTable('workTime');
+//   }
+// };
