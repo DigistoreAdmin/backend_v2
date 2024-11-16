a6282967fc7a206dcebccfd7840c33f864df2775
Aravind P <araviindp@gmail.com>
2024-11-16 11:19:21 +0530

    monthly-commission

--- route/index.js
+++ route/index.js
@@ -62,7 +62,7 @@ const checkIntentStatusRoute = require("../route/upiPaymentRoute/checkIntentStat
 const checkStatusRoute =require("../route/upiPaymentRoute/checkStatusRoute")
 const payOrderRoute = require("../route/upiPaymentRoute/payOrderRoute")
 const upiIntentRoute = require("../route/upiPaymentRoute/upiIntentRoute")
-
+const getMonthlyCommissionsRoute=require("./accountantRoute/monthlyCommisionRoute")
 
 
 
@@ -136,7 +136,7 @@ router.use("/v1/upiPayment", checkIntentStatusRoute)
 router.use("/v1/checkStatusRoute",checkStatusRoute)
 router.use("/v1/payOrder",payOrderRoute )
 router.use("/v1/upiIntent",upiIntentRoute )
-
+router.use("/v1/MonthlyCommissions",getMonthlyCommissionsRoute)
 
 
 
